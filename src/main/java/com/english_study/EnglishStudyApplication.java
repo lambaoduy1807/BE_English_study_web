0d73f49a5cf9c278465321622e77ed6e0aa172b0
lambaoduy <22130057@st.hcmuaf.edu.vn>
2026-03-21 16:20:09 +0700

    init project

---
+++ src/main/java/com/english_study/EnglishStudyApplication.java
@@ -0,0 +1,13 @@
+package com.english_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishStudyApplication.class, args);
+    }
+
+}
